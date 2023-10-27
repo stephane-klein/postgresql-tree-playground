c32552efb14645062b0ef1304e7caa15d598e58d
Stéphane Klein <contact@stephane-klein.info>
2023-10-27 18:19:53 +0200

    Add some folders in the fixture

--- sqls/fixtures.sql
+++ sqls/fixtures.sql
@@ -18,17 +18,41 @@ INSERT INTO public.folders
         1,
         NULL
     ),
-    (
-        2,
-        'folder_aa',
-        1,
-        1
-    ),
-    (
-        3,
-        'folder_aaa',
-        1,
-        2
-    );
+        (
+            2,
+            'folder_aa',
+            1,
+            1
+        ),
+            (
+                3,
+                'folder_aaa',
+                1,
+                2
+            ),
+            (
+                4,
+                'folder_aab',
+                2,
+                2
+            ),
+            (
+                5,
+                'folder_aac',
+                3,
+                2
+            ),
+        (
+            6,
+            'folder_ab',
+            2,
+            1
+        ),
+        (
+            7,
+            'folder_ac',
+            3,
+            1
+        );
 
 \echo "Fixtures loaded"
