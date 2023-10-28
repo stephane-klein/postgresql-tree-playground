24907028353d0cfcd32bb11efb61123d46511d9a
Stéphane Klein <contact@stephane-klein.info>
2023-10-28 10:30:29 +0200

    Improve fixture coding style

--- sqls/fixtures.sql
+++ sqls/fixtures.sql
@@ -4,55 +4,13 @@ SET client_min_messages TO WARNING;
 
 TRUNCATE public.folders;
 
-INSERT INTO public.folders
-    (
-        id,
-        name,
-        position,
-        parent_id
-    )
-    VALUES
-    (
-        1,
-        'folder_a',
-        1,
-        NULL
-    ),
-        (
-            2,
-            'folder_aa',
-            1,
-            1
-        ),
-            (
-                3,
-                'folder_aaa',
-                1,
-                2
-            ),
-            (
-                4,
-                'folder_aab',
-                2,
-                2
-            ),
-            (
-                5,
-                'folder_aac',
-                3,
-                2
-            ),
-        (
-            6,
-            'folder_ab',
-            2,
-            1
-        ),
-        (
-            7,
-            'folder_ac',
-            3,
-            1
-        );
+INSERT INTO public.folders (id, name, position, parent_id) VALUES
+    (1, 'folder_a', 1, NULL),
+        (2, 'folder_aa', 1, 1),
+            (3, 'folder_aaa', 1, 2),
+            (4, 'folder_aab', 2, 2),
+            (5, 'folder_aac', 3, 2),
+        (6, 'folder_ab', 2, 1),
+        (7, 'folder_ac', 3, 1);
 
 \echo "Fixtures loaded"
